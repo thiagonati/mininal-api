bb95cad9845de9fe1758e4bc349fd0ae817c2222
thiagonati <72274069+thiagonati@users.noreply.github.com>
2025-10-19 14:03:49 -0300

    feat: criando entidade e conxeão com banco de dados

--- Program.cs
+++ Program.cs
@@ -1,6 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 using MInimalApi.DTOs;
+using MInimalApi.Infraestrutura.Db;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<DbContexto>(
+    options => options.UseMySql(
+        builder.Configuration.GetConnectionString("mysql"),
+        ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("mysql"))
+    )
+);
 var app = builder.Build();
 
 app.MapGet("/", () => "hello world");
