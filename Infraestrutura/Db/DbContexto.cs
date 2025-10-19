86d36a2b816fd87e5fade2ca274b596899304d3e
thiagonati <72274069+thiagonati@users.noreply.github.com>
2025-10-19 14:04:48 -0300

    feat: criando migration e seed

--- Infraestrutura/Db/DbContexto.cs
+++ Infraestrutura/Db/DbContexto.cs
@@ -1,6 +1,41 @@
+using Microsoft.EntityFrameworkCore;
+using miminal_api.Migrations;
+using MInimalApi.Dominio.Entidades;
+
 namespace MInimalApi.Infraestrutura.Db;
 
-public class DbContexto
+public class DbContexto : DbContext
 {
-    public string ConnectionString { get; set; }
+    private readonly IConfiguration _configuration;
+    public DbContexto(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+    public DbSet<Administrador> Administradores { get; set; } = default!;
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Administrador>().HasData(
+            new Administrador()
+            {
+                Id = 1,
+                Email = "adm@example.com",
+                Senha = "123456",
+                Perfil = "Adm"
+            }
+        );
+
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            var connectionString = _configuration.GetConnectionString("mysql")?.ToString();
+            if (!string.IsNullOrEmpty(connectionString))
+            {
+                optionsBuilder.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+            }
+        }
+    }
 }
